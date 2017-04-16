618c89400700ed6067407781efeba44d072d5cde
viktor <vikand.polpob@gmailcom>
2017-04-16 14:09:28 +0300

    Новые файлы

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test flexbox</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="wrapper">
+	<div class="content">
+		<ul>
+			<li><a href="#">Об авторе</a></li>
+			<li><a href="#">Творчество</a></li>
+			<li><a href="#">Книги</a></li>
+
+		</ul>
+		
+	</div><!--End Content-->
+</div><!--End Wrapper->
+</body>
+</html>
